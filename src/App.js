ed65ba010bf897e75761cda2bd70c02bc768977f
jaggarinikhil <jaggarinikhil@gmail.com>
2024-06-02 15:59:16 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
+import "."
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h className=" text-2xl text-red-600 m-5 p-5">welcome to new project</h>
     </div>
   );
 }
